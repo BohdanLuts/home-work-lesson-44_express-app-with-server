1ed1ef27df449a7fd7e8b421ec99b4b65c45ef32
Bohdan <theodoronos@gmail.com>
2023-04-25 19:00:31 +0200

    Add get, patch, delete task by id

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ const tasksDB = [
     createdAt: format(new Date(), "'Created at 'Y.MM.dd eeee,' at 'HH:mm"),
     isDone: true,
   },
+  {
+    id: 3,
+    task: 'Wash the dishes',
+    createdAt: format(new Date(), "'Created at 'Y.MM.dd eeee,' at 'HH:mm"),
+    isDone: true,
+  },
+  {
+    id: 4,
+    task: 'Walking with dog',
+    createdAt: format(new Date(), "'Created at 'Y.MM.dd eeee,' at 'HH:mm"),
+    isDone: true,
+  },
 ];
 class TasksDB {
   constructor (arr) {
     this.tasks = [...arr];
-    1;
   }
-  createTask (newContact) {
+  createTask (newTask) {
     this.tasks.push({ ...newTask, id: uuidv4(), isDone: false });
     return this.tasks[this.tasks.length - 1];
   }
@@ -28,6 +39,7 @@ class TasksDB {
     return [...this.tasks];
   }
 }
+
 const tasksDbInstance = new TasksDB(tasksDB);
 
 const app = express();
@@ -46,6 +58,49 @@ app.get('/tasks', (req, res) => {
 app.post('/tasks', (req, res) => {
   const createdTask = tasksDbInstance.createTask(req.body);
   res.status(201).send(createdTask);
+});
+
+// 1. Реалізувати запит на перегляд однієї таски:
+app.get('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const task = tasksDbInstance.getTasks().find(t => t.id === taskId);
+  if (!task) {
+    res.status(404).send('Task not found');
+  } else {
+    res.status(200).send(task);
+  }
+});
+
+// 2. Реалізувати запит на оновлення однієї таски:
+app.patch('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const updatedTask = req.body;
+  const taskIndex = tasksDbInstance.tasks.findIndex(task => task.id === taskId);
+
+  if (taskIndex === -1) {
+    res.status(404).send('Task not found');
+    return;
+  }
+
+  tasksDbInstance.tasks[taskIndex] = {
+    ...tasksDbInstance.tasks[taskIndex],
+    ...updatedTask,
+    id: taskId,
+  };
+
+  res.status(200).send(tasksDbInstance.tasks[taskIndex]);
+});
+
+// 3. Реалізувати запит на видалення однієї таски:
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const taskIndex = tasksDbInstance.tasks.findIndex(task => task.id === taskId);
+  if (taskIndex === -1) {
+    res.status(404).send('Task not found');
+    return;
+  }
+  tasksDbInstance.tasks.splice(taskIndex, 1);
+  res.status(200).send('Task deleted');
 });
 
 module.exports = app;
